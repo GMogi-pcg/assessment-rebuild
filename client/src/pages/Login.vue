31877354481b9fb3912be72d63165f2c98bc48ad
GMogi-pcg <GMogi@phxcapitalgroup.com>
2024-09-25 11:00:53 -0700

    starting working on login page for frontend

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,8 +1,15 @@
+// need to add logic to handle the form submission
+
 <template>
-  <div>
-    <h1>Login</h1>
-    <input v-model="email" placeholder="Email">
-    <input v-model="password" placeholder="Password" type="password">
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-10">
+    <h1 class="text-3xl sm:text-4xl lg-text-5xl font-bold mb-8 text-center text-gray-800">Login</h1>
+    <form class="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
+      <div class="mb-4">
+        <label htmlFor="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+        <input v-model="email" type="email" id="email" class="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" />
+      </div>
+    </form>
+
   </div>
 </template>
 
