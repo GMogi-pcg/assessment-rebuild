e23bc05175ef92d8fe671137a94028742eb3077f
GMogi-pcg <GMogi@phxcapitalgroup.com>
2024-09-30 14:42:09 -0700

    fixing build for hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: 'client', // Set the root directory to 'client'
+  build: {
+    outDir: '../dist', // Place the build output in a folder accessible by Cloudflare Pages
+  },
 })
